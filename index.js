0854319d8b5ce48a500eaece8572f35a261b5d19
Jasmine McManus <jasmine.mcmanus@outlook.com>
2023-05-25 21:43:02 +1000

    done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if(feet <=400) {
+    return 'This one is on me!';
+  }
+  else if(feet <=2000) {
+    return 'That will be twenty bucks.';
+  }
+  else if (feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC'?'Ok, sounds good.' :'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip) {
+      case 'generous':
+        return 'Thank you so much.'
+      case 'not as generous':
+        return 'Thank you.'
+      default:
+        return 'Bye.'
+  }
 }
+
+console.log(scuberGreetingForFeet(199));
+console.log(scuberGreetingForFeet(1500));
+console.log(scuberGreetingForFeet(2001));
+console.log(scuberGreetingForFeet(2501));
+
+console.log(ternaryCheckCity('NYC'));
+console.log(ternaryCheckCity('Los Angeles'));
+
+console.log(switchOnCharmFromTip('generous'));
+console.log(switchOnCharmFromTip('not as generous'));
+console.log(switchOnCharmFromTip('nothing'));
